023f69d6b097df501331729477726b4f3fc0cfa2
Daniel Ferreira <djf989@gmail.com>
2017-11-28 03:40:49 -0200

    added supported language codes

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import           Control.Monad         (when)
+import           Control.Monad         (unless, when)
 import           Hassub
 import           System.Console.Docopt
 import           System.Directory
@@ -38,7 +38,9 @@ main = do
         when (isPresent args (shortOption 'v')) $ putStrLn version >> exitSuccess
 
         let (Just lang) = args `getArg` shortOption 'l'
-            silent = isPresent args (shortOption 's')
+        unless (lang `elem` supportedLangs) $ putStrLn ("Unknown language: "++ lang ++". Use ISO 639-2 codes") >> exitSuccess
+
+        let silent = isPresent args (shortOption 's')
 
         files <- getFiles args
 
@@ -55,3 +57,54 @@ getFiles args = if isPresent args (longOption "all") then do
 
 supportedExt :: [String]
 supportedExt = ["mkv", "avi", "mp4"]
+
+supportedLangs :: [String]
+supportedLangs = ["aar", "abk", "ace", "ach", "ada", "ady", "afa", "afh", "afr", "ain",
+                  "aka", "akk", "alb", "ale", "alg", "alt", "amh", "ang", "apa", "ara",
+                  "arc", "arg", "arm", "arn", "arp", "art", "arw", "asm", "ast", "ath",
+                  "aus", "ava", "ave", "awa", "aym", "aze", "bad", "bai", "bak", "bal",
+                  "bam", "ban", "baq", "bas", "bat", "bej", "bel", "bem", "ben", "ber",
+                  "bho", "bih", "bik", "bin", "bis", "bla", "bnt", "bos", "bra", "bre",
+                  "btk", "bua", "bug", "bul", "bur", "byn", "cad", "cai", "car", "cat",
+                  "cau", "ceb", "cel", "cha", "chb", "che", "chg", "chi", "chk", "chm",
+                  "chn", "cho", "chp", "chr", "chu", "chv", "chy", "cmc", "cop", "cor",
+                  "cos", "cpe", "and", "cpf", "and", "cpp", "and", "cre", "crh", "crp",
+                  "and", "csb", "cus", "cze", "dak", "dan", "dar", "day", "del", "den",
+                  "dgr", "din", "div", "doi", "dra", "dua", "dum", "dut", "dyu", "dzo",
+                  "efi", "egy", "eka", "elx", "eng", "enm", "epo", "est", "ewe", "ewo",
+                  "fan", "fao", "fat", "fij", "fil", "fin", "fiu", "fon", "fre", "frm",
+                  "fro", "fry", "ful", "fur", "gaa", "gay", "gba", "gem", "geo", "ger",
+                  "gez", "gil", "gla", "gle", "glg", "glv", "gmh", "goh", "gon", "gor",
+                  "got", "grb", "grc", "ell", "grn", "guj", "gwi", "hai", "hat", "hau",
+                  "haw", "heb", "her", "hil", "him", "hin", "hit", "hmn", "hmo", "hrv",
+                  "hun", "hup", "iba", "ibo", "ice", "ido", "iii", "ijo", "iku", "ile",
+                  "ilo", "ina", "inc", "ind", "ine", "inh", "ipk", "ira", "iro", "ita",
+                  "jav", "jpn", "jpr", "jrb", "kaa", "kab", "kac", "kal", "kam", "kan",
+                  "kar", "kas", "kau", "kaw", "kaz", "kbd", "kha", "khi", "khm", "kho",
+                  "kik", "kin", "kir", "kmb", "kok", "kom", "kon", "kor", "kos", "kpe",
+                  "krc", "kro", "kru", "kua", "kum", "kur", "kut", "lad", "lah", "lam",
+                  "lao", "lat", "lav", "lez", "lim", "lin", "lit", "lol", "loz", "ltz",
+                  "lua", "lub", "lug", "lui", "lun", "luo", "and", "lus", "mac", "mad",
+                  "mag", "mah", "mai", "mak", "mal", "man", "mao", "map", "mar", "mas",
+                  "may", "mdf", "mdr", "men", "mga", "mic", "min", "mis", "mkh", "mlg",
+                  "mlt", "mnc", "mni", "mno", "moh", "mol", "mon", "mos", "mwl", "mul",
+                  "mun", "mus", "mwr", "myn", "myv", "nah", "nai", "nap", "nau", "nav",
+                  "nbl", "nde", "ndo", "nds", "nep", "new", "nia", "nic", "niu", "nno",
+                  "nob", "nog", "non", "nor", "nso", "nub", "nwc", "nya", "nym", "nyn",
+                  "nyo", "nzi", "oci", "oji", "ori", "orm", "osa", "oss", "ota", "oto",
+                  "paa", "pag", "pal", "pam", "pan", "pap", "pau", "peo", "per", "phi",
+                  "phn", "pli", "pol", "pon", "por", "pra", "pro", "pus", "que", "raj",
+                  "rap", "rar", "roa", "roh", "rom", "run", "rup", "rus", "sad", "sag",
+                  "sah", "sai", "sal", "sam", "san", "sas", "sat", "scc", "scn", "sco",
+                  "sel", "sem", "sga", "sgn", "shn", "sid", "sin", "sio", "sit", "sla",
+                  "slo", "slv", "sma", "sme", "smi", "smj", "smn", "smo", "sms", "sna",
+                  "snd", "snk", "sog", "som", "son", "sot", "spa", "srd", "srr", "ssa",
+                  "ssw", "suk", "sun", "sus", "sux", "swa", "swe", "syr", "tah", "tai",
+                  "tam", "tat", "tel", "tem", "ter", "tet", "tgk", "tgl", "tha", "tib",
+                  "tig", "tir", "tiv", "tkl", "tlh", "tli", "tmh", "tog", "ton", "tpi",
+                  "tsi", "tsn", "tso", "tuk", "tum", "tup", "tur", "tut", "tvl", "twi",
+                  "tyv", "udm", "uga", "uig", "ukr", "umb", "und", "urd", "uzb", "vai",
+                  "ven", "vie", "vol", "vot", "wak", "wal", "war", "was", "wel", "wen",
+                  "wln", "wol", "xal", "xho", "yao", "yap", "yid", "yor", "ypk", "zap",
+                  "zen", "zha", "znd", "zul", "zun", "rum", "pob", "mne", "zht", "zhe",
+                  "pom", "ext"]
